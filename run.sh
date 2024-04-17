a193014e9b17257d877ec3ea9daaa53c16f62fb6
Nikos Delis <contact@nikosdelis.com>
2024-04-17 19:56:12 +0200

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 sudo apt-get update && \
-  sudo apt-get install -y dotnet-runtime-8.0
+  sudo apt-get install -y dotnet-sdk-8.0
 
-export IOTHUB_DEVICE_CONNECTION_STRING=<CONNECTIONSTRING>
+export IOTHUB_DEVICE_CONNECTION_STRING="<CONNECTIONSTRING>"
 dotnet run
